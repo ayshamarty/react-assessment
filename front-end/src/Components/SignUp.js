f7860fd2a6bc9432f23c75bdf6e2250bb119926f
ayshamarty <ayshamarty@gmail.com>
2019-08-02 10:22:00 +0100

    set up structure of project

---
+++ front-end/src/Components/SignUp.js
@@ -0,0 +1,16 @@
+import React, {Component} from "react";
+
+class SignUp extends Component {
+  constructor() {
+    super();
+    this.state = {
+      username: "",
+      email: [],
+      password: "",
+      password2: ""
+    };
+  }
+
+}
+
+export default SignUp;
---
+++ front-end/src/Components/UserList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ListGroup from "react-bootstrap/ListGroup";
+
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+import User from "./User";
+
+function UserList(props) {
+
+}
+
+export default UserList
